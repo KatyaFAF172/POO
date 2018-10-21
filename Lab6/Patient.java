d3cf0147d72f9cfa049335628262e070acaf5a45
KatyaFAF172 <42991993+KatyaFAF172@users.noreply.github.com>
2018-10-21 14:07:30 +0300

    Lab6

---
+++ Lab6/Patient.java
@@ -0,0 +1,15 @@
+package lab6;
+
+public class Patient extends Person {
+    String id;
+    FullName name;
+    Gender gender;
+    Date birthDate;
+    Integer age;
+    Date accepted;
+    History sickness;
+    String [] prescriptions;
+    String [] allergies;
+    String [] specialReqs;
+
+}
---
+++ Lab6/Staff.java
@@ -0,0 +1,8 @@
+package lab6;
+
+public class Staff {
+    Date joined;
+    String [] education;
+    String [] certification;
+    String [] languages;
+}
