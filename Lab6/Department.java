c79e704b4b6d7b037433542f37b6e397aa06a8d4
KatyaFAF172 <42991993+KatyaFAF172@users.noreply.github.com>
2018-10-21 22:00:19 +0300

    Lab6

--- Lab6/Department.java
+++ Lab6/Department.java
@@ -1,4 +1,7 @@
 package lab6;
 
+import java.util.Vector;
+
 public class Department {
+    private Vector Staff; //List of all Staff
 }
--- Lab6/Hospital.java
+++ Lab6/Hospital.java
@@ -1,6 +1,9 @@
 package lab6;
 
+import java.util.Vector;
+
 public class Hospital {
+    private Vector Department; //List of all Department
     String name;
     Address address;
     Phone phone;
